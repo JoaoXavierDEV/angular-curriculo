f31c8e91b5b57902835c408dd660ceea21252b54
joaofernando <joao_jfmx@outlook.com>
2021-07-20 17:02:50 -0300

    1.0.2

--- src/app/views/projetos/projetos.component.ts
+++ src/app/views/projetos/projetos.component.ts
@@ -5,6 +5,8 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './projetos.component.html',
   styleUrls: ['./projetos.component.scss']
 })
+  
+
 export class ProjetosComponent implements OnInit {
   title: string = 'Projetos';
 
@@ -13,5 +15,28 @@ export class ProjetosComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  
+
+  public projetos = [{
+    nome: 'AutonomoApp',
+    linguagem: 'Flutter - App Android',
+    descricao: 'App desenvolvido para unir pessoas autônomas a pessoas que buscam algum tipo de serviço.** Projeto criado para fins de aprendizado**',
+    img: 'https://miro.medium.com/max/2000/1*PCKC8Ufml-wvb9Vjj3aaWw.jpeg',
+    github: 'https://github.com/joaojfmx/Autonomo_app',
+  }, {
+    nome: 'Site Currículo',
+    linguagem: 'Angular',
+    descricao: 'Desenvolvido em CSS Grid/Flex, Angular Material.',
+    img: 'https://miro.medium.com/max/2000/1*_UY7U-Umohxg-88is_e3mQ.png',
+    github: 'https://github.com/joaojfmx/angular-curriculo',
+    },
+    {
+      nome: 'CestaFácil',
+      linguagem: 'Ionic - App Android',
+      descricao: 'Projeto final feito em 2019 no curso técnico SENAC.',
+      img: 'https://www.mundojs.com.br/wp-content/uploads/2018/09/ionic-framework-og.png',
+      github: 'https://github.com/joaojfmx/ionic_4-cestafacil',
+    },
+  ];
 
 }
